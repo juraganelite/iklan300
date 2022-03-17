a7acc5e00898e57d0e798cc4c8b77b328ac25d33
juraganelite <101795987+juraganelite@users.noreply.github.com>
2022-03-17 17:17:21 +0700

    Add files via upload

---
+++ iklan300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'bade0b1ea33c24096f9f72959e267fb2',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://shriekdestitute.com/bade0b1ea33c24096f9f72959e267fb2/invoke.js"></scr' + 'ipt>');
